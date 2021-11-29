8440dcb2eae326e7bc617a66be3fe9952e3a23f3
Kristi Kozak <kristi@kozaked.com>
2021-11-29 04:39:57 -0500

    update to get all params, not hit 10 max

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import {GetParametersByPathCommand, SSMClient} from '@aws-sdk/client-ssm'
+import {GetParametersByPathCommand, SSMClient, GetParametersByPathCommandOutput} from '@aws-sdk/client-ssm'
 import {writeFileSync} from 'fs'
 
 async function run(): Promise<void> {
@@ -14,67 +14,70 @@ async function run(): Promise<void> {
 
     const params = {} as any
 
-    while (true) {
-      for (const path of paths) {
-        let NextToken: string | undefined
-        const result = await ssm.send(
+    for (const path of paths) {
+      let NextToken: string | undefined
+      let result: GetParametersByPathCommandOutput
+      while (true) {
+        result = await ssm.send(
           new GetParametersByPathCommand({
             Path: path,
             Recursive: JSON.parse(core.getInput('recursive', {required: true})),
             WithDecryption: JSON.parse(
               core.getInput('decrypt', {required: true})
             ),
-            NextToken
+            NextToken,
+            MaxResults: 10
           })
         )
-
-        if (saveToEnvironment) {
-          // eslint-disable-next-line i18n-text/no-en
-          core.startGroup(`Exporting from Path ${path}`)
-        }
-
-        for (const parameter of result.Parameters!) {
-          let name = parameter.Name?.replace(path, '')
-            .toUpperCase()
-            .replace(/\//g, delimiter)
-            .replace(/-/g, '_')
-          name = `${prefix}${name}`
-          params[name] = parameter.Value
-          if (saveToEnvironment) {
-            // eslint-disable-next-line i18n-text/no-en
-            core.info(`Exporting variable ${name}`)
-            core.info(`NextToken ${NextToken}`)
-            core.exportVariable(name, parameter.Value)
-            if (parameter.Value && parameter.Type === 'SecureString') {
-              core.setSecret(parameter.Value)
-            }
-          }
-        }
+        core.info(`NextToken ${result.NextToken}`)
         NextToken = result.NextToken
         if (!NextToken) {
           break
         }
       }
+
+      if (saveToEnvironment) {
+        // eslint-disable-next-line i18n-text/no-en
+        core.startGroup(`Exporting from Path ${path}`)
+      }
+
+      for (const parameter of result.Parameters!) {
+        let name = parameter.Name?.replace(path, '')
+          .toUpperCase()
+          .replace(/\//g, delimiter)
+          .replace(/-/g, '_')
+        name = `${prefix}${name}`
+        params[name] = parameter.Value
+        if (saveToEnvironment) {
+          // eslint-disable-next-line i18n-text/no-en
+          core.info(`Exporting variable ${name}`)
+          core.exportVariable(name, parameter.Value)
+          if (parameter.Value && parameter.Type === 'SecureString') {
+            core.setSecret(parameter.Value)
+          }
+        }
+      }
+
       if (saveToEnvironment) {
         core.endGroup()
       }
-
-      const fileName = core.getInput('file', {required: false})
-      core.info(fileName)
-      if (fileName) {
-        // eslint-disable-next-line i18n-text/no-en
-        core.info(`Writing to file ${fileName}`)
-
-        writeFileSync(
-          fileName,
-          Object.keys(params)
-            .map(k => `${k}=${params[k]}`)
-            .join('\n')
-        )
-      }
-
-      core.setOutput('ssm-params', JSON.stringify(params))
     }
+
+    const fileName = core.getInput('file', {required: false})
+    core.info(fileName)
+    if (fileName) {
+      // eslint-disable-next-line i18n-text/no-en
+      core.info(`Writing to file ${fileName}`)
+
+      writeFileSync(
+        fileName,
+        Object.keys(params)
+          .map(k => `${k}=${params[k]}`)
+          .join('\n')
+      )
+    }
+
+    core.setOutput('ssm-params', JSON.stringify(params))
   } catch (error) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
